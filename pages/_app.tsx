50db8a058bfdc4c8203272a5fc6ee3d5dbec4486
Maksim Lisianskii <Max_Lisyanskiy@mail.ru>
2022-01-05 22:08:08 +0300

    added firts Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <>
+    <Head>
+      <title>My Top App</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
